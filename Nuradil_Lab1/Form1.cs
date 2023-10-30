b9574565d7ac861e8d29a3ff92a7717ba4b55cde
mataraimov <minuradil@gmail.com>
2023-10-30 11:44:50 +0600

    lab uploads

---
+++ Nuradil_Lab1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(textBox1.Text);
+            int second = Convert.ToInt32(textBox2.Text);
+            string operator1 = textBox3.Text;
+            if (operator1 == "-")
+            {
+                textBox4.Text = (first - second).ToString();
+            }
+            else if (operator1 == "+") {
+                textBox4.Text = (first + second).ToString();
+            }
+            else if (operator1 == "/")
+            {
+                textBox4.Text = (first / second).ToString();
+            }
+            else if (operator1 == "*")
+            {
+                textBox4.Text = (first * second).ToString();
+            }
+        }
+
+       
+    }
+}
